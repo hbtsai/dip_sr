d7928311e02cfbf8878054d17cfd4e2568db78e5
mavewei <mavewei@gmail.com>
2013-12-20 23:18:39 +0800

    First commit

---
+++ BuildINumber.h
@@ -0,0 +1,38 @@
+/*
+Filename:   BuildINumber.h
+Author:     Mave Yeap
+Date:       Dec,2013
+[DESCRIPTION]: BuildINumber header file.
+*/
+
+#ifndef	__BuildINumber_H__
+#define	__BuildINumber_H__
+
+/**********************
+*	Standard headers
+**********************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <cv.h>
+#include <highgui.h>
+
+/**********************
+*	Program headers
+**********************/
+
+
+
+
+/**********************
+*	Program structure
+**********************/
+
+
+
+/**********************
+*	Program Define
+**********************/
+
+
+#endif
